a4b935e5646bc88496db76157768cfe0d7ef83fe
Josue <78059379+jlemus1367@users.noreply.github.com>
2021-06-11 23:31:56 -0500

    Uploading deliverable 3 material

--- Resources/MechaCarChallenge.R
+++ Resources/MechaCarChallenge.R
@@ -14,7 +14,6 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 #----------Deliverable 2----------#
-
 #Import and view CSV file
 Suspension_Coil <- read_csv("Suspension_Coil.csv")
 View(Suspension_Coil)
@@ -26,3 +25,17 @@ View(total_summary)
 #Summarize data by lot
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 View(lot_summary)
+
+
+#----------Deliverable 3----------#
+# T-Test comparing all manufacturing lots to population mean
+t.test(Suspension_Coil$PSI,mu=1500)
+
+# T-Test comparing Lot 1 to population mean
+t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot1')$PSI, mu=1500)
+
+# T-Test comparing Lot 2 to population mean
+t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot2')$PSI, mu=1500)
+
+#T-Test comparing Lot 3 to population mean
+t.test(subset(Suspension_Coil, Manufacturing_Lot == 'Lot3')$PSI, mu=1500)
