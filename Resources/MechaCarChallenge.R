890bd9fb3f335a8a48590b47e59d12b4118ac8b5
Josue <78059379+jlemus1367@users.noreply.github.com>
2021-06-11 21:05:26 -0500

    Uploading material for deliverable 2

--- Resources/MechaCarChallenge.R
+++ Resources/MechaCarChallenge.R
@@ -1,7 +1,8 @@
+#-----------Deliverable 1----------#
 # Upload tidyverse package
 library(dplyr)
 
-# Import CSV file and view csv file
+# Import and view CSV file
 MechaCar_mpg <- read_csv("MechaCar_mpg.csv")
 View(MechaCar_mpg)
 
@@ -10,3 +11,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+
+#----------Deliverable 2----------#
+
+#Import and view CSV file
+Suspension_Coil <- read_csv("Suspension_Coil.csv")
+View(Suspension_Coil)
+
+#Summarize data
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+View(total_summary)
+
+#Summarize data by lot
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+View(lot_summary)
